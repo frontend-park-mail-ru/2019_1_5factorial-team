a870f690cd9dcb0fae95740b3b479c5508957822
4taa <heartaccount@rambler.ru>
2019-02-19 10:54:43 +0300

    changed get requests

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-var http = require('http'),
-    express = require('express'),
-    app = express();
+const http = require('http'),
+      express = require('express'),
+      app = express();
 
 http.createServer(app).listen(3000);
 console.log('Server started at' + ' http://127.0.0.1:3000');
 
-app.get('/', function (req, res) {                       //Пока что это будет страничка меню, ибо все равно стартовая
+
+app.get('/', (req, res) => {
     res.sendFile('./static/index.html', {root: __dirname});
     console.log('GET index');
 });
 
-app.get('/login', function (req, res) {
+app.get('/login', (req, res) => {
     res.sendFile('./static/login.html', {root: __dirname});
     console.log('GET login');
 });
 
-app.get('/registration', function (req, res) {
-    res.sendFile('./static/registration.html', {root: __dirname});
-    console.log('GET registration');
-});
-
-app.get('/about', function (req, res) {
+app.get('/about', (req, res) => {
     res.sendFile('./static/about.html', {root: __dirname});
     console.log('GET about');
 });
 
-app.get('/profile', function (req, res) {
-    res.sendFile('./static/profile.html', {root: __dirname});
-    console.log('GET profile');
-});
-
-app.get('/leaderboard', function (req, res) {
+app.get('/leaderboard', (req, res) => {
     res.sendFile('./static/leaderboard.html', {root: __dirname});
     console.log('GET leaderboard');
+});
+
+app.get('/profile', (req, res) => {
+    res.sendFile('./static/Profile.html', {root: __dirname});
+    console.log('GET Profile');
+});
+
+app.get('/registration', (req, res) => {
+    res.sendFile('./static/registration.html', {root: __dirname});
+    console.log('GET registration');
 });
 
 app.use(express.static(__dirname + '/static'));
