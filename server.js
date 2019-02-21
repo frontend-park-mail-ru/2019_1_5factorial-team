196f0e84bc9502684448574340a80c8243c2d7de
smet_k <ski97@mail.ru>
2019-02-21 16:46:19 +0300

    upload avatar html

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const http = require('http'),
       express = require('express'),
       app = express();
+      body = require('body-parser');
 
 http.createServer(app).listen(3000);
 console.log('Server started at' + ' http://127.0.0.1:3000');
 
+
+let users = {
+    'kek': {
+        email: 'kek.k.ek',
+		password: 'password',
+		age: 1,
+        score: 100500,
+        avatar: 'default',
+        avatarLink: `./static/avatars/${avatar}.jpg`
+    }, 
+    'lol': {
+        email: 'lol.l.ol',
+		password: 'password',
+		age: 1,
+        score: 100500,
+        avatar: 'default',
+        avatarLink: `./static/avatars/${avatar}.jpg`
+    }, 
+};
 
 app.get('/', (req, res) => {
     res.sendFile('./static/index.html', {root: __dirname});
@@ -30,6 +50,11 @@ app.get('/profile', (req, res) => {
     res.sendFile('./static/Profile.html', {root: __dirname});
     console.log('GET Profile');
 });
+
+app.post('/profile', (req, res) => {
+    // const usr = req.body.email;
+    console.log(req)
+})
 
 app.get('/registration', (req, res) => {
     res.sendFile('./static/registration.html', {root: __dirname});
