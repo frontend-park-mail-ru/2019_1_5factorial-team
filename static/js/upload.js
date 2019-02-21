e789f0fe11ef96673af9366af21bf0babcfadd73
smet_k <ski97@mail.ru>
2019-02-21 17:41:13 +0300

    save

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,1 +1,13 @@
 // kek
+const app = document.getElementById('app');
+
+const inputPhotoForm = document.getElementById('upload-photo');
+
+const submitUpload = document.getElementsByClassName('btn btn-default')[0];
+
+submitUpload.addEventListener('click', (event) => {
+    console.log(event);
+    event.preventDefault();
+
+    alert('111');
+});
