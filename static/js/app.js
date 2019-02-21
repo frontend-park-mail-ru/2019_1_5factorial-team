0e89e5e00ad342e5ae90185e5af6caf227963e45
4taa <heartaccount@rambler.ru>
2019-02-21 15:23:10 +0300

    added validator class

--- static/js/app.js
+++ static/js/app.js
@@ -1,38 +1,63 @@
 'use strict';
 
-const form = document.getElementsByClassName('form');
+class Validator {
 
-const validateEmail = (email) => {
-    let expression = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/);
-    let res = expression.test(email);
-    if (res) {
-        console.log('correct email!');
-        return true
-    }
-    console.log('incorrect email!');
-    return false
-};
+    validateUser = (user, pass) => {
+        if (this.validateEmail(user)) {
+            if (this.validatePassword(pass)) {
+                console.log('enter');
+                return true;
+            }
+        } else if (this.validateLogin(user)) {
+            if (this.validatePassword(pass)) {
+                console.log('enter');
+                return true;
+            }
+        } else {
+            console.log('ploho');
+            return false;
+        }
+    };
 
-const validateLogin = (login) => {
-    if (myInput.value.length >= 8) {
-        length.classList.remove("invalid");
-        length.classList.add("valid");
-        console.log('correct login!');
-    } else {
-        length.classList.remove("valid");
-        length.classList.add("invalid");
-        console.log('incorrect login!');
-    }
-};
+    validateEmail = email => {
+        let expression = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+[^<>()\.,;:\s@\"]{2,})$/);
+        let res = expression.test(email);
+        if (res) {
+            console.log('correct email!');
+            return true
+        }
+        console.log('incorrect email!');
+        return false
+    };
 
-const validatePassword = (password) => {
-    if (myInput.value.length >= 8) {
-        length.classList.remove("invalid");
-        length.classList.add("valid");
-        console.log('correct password!');
-    } else {
-        length.classList.remove("valid");
-        length.classList.add("invalid");
-        console.log('incorrect password!');
-    }
-};
+    validateLogin = login => {
+        const myInput = document.getElementsByName('login-or-email');
+        if (login.length >= 8) {
+            // length.classList.remove("invalid");
+            // length.classList.add("valid");
+            console.log('correct login!');
+            return true
+        } else {
+            // length.classList.remove("valid");
+            // length.classList.add("invalid");
+            console.log('incorrect login!');
+            return false
+        }
+    };
+
+    validatePassword = password => {
+        const myInput = document.getElementsByName('password');
+        if (password.length >= 8) {
+            // length.classList.remove("invalid");
+            // length.classList.add("valid");
+            console.log('correct password!');
+            return true
+        } else {
+            // length.classList.remove("valid");
+            // length.classList.add("invalid");
+            console.log('incorrect password!');
+            return false
+        }
+    };
+}
+
