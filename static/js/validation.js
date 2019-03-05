a58a1dd2021f94f0dd83501631069457e36cbb65
4taa <heartaccount@rambler.ru>
2019-03-05 18:06:23 +0300

    keep merging changing-avatr-smet1 to dev

--- static/js/validation.js
+++ static/js/validation.js
@@ -102,26 +102,3 @@ export default class Validator {
         }
     }
 }
-// Temp DOM's elems for future validation!
-// const avatarInput = document.getElementsByClassName('js-avatar')[0];
-// const firstPassInput = document.getElementsByClassName('js-password')[0];
-// const secondPassInput = document.getElementsByClassName('js-password')[1];
-
-const submit = document.getElementsByClassName('js-submit')[0];
-submit.addEventListener('click', (event) => {
-    event.preventDefault();
-    const passInput = document.getElementsByClassName('js-password')[0];
-    const logInput = document.getElementsByClassName('js-login-or-email')[0];
-
-    const validate = new Validator();
-    const checkLogin = validate.validateLogin(logInput);
-    const checkPass = validate.validatePassword(passInput);
-    const checkEmail = validate.validateEmail(logInput);
-    if (checkLogin && checkPass) {
-        console.log('Success!');
-    } else if (checkEmail && checkPass) {
-        console.log('Success!');
-    } else {
-        console.log('Smth went wrong!');
-    }
-});
