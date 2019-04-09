1efea2c61c34a967fca7c62f3bd7079d4af998e6
4taa <heartaccount@rambler.ru>
2019-04-09 15:23:43 +0300

    addded player

--- static/js/components/game/gameScene.js
+++ static/js/components/game/gameScene.js
@@ -4,33 +4,48 @@ export default class gameScene {
         this.ghosts = ghosts;
         this.state = null;
         this.bindedResizer = this.resizer.bind(this);
+
         window.addEventListener('resize', this.bindedResizer);
         this.resizer();
 
-        console.log(this.canvas);
         this.ctx = this.canvas.getContext('2d');
         this.renderScene = this.renderScene.bind(this);
     }
 
     renderScene(now) {
-        const ctx = this.ctx;
-        // const scene = this.scene;
         this.lastFrameTime = now;
         console.log('got inside gs');
+        this.initGhosts(this.ghosts);
+        this.initPlayer(this.player);
+
+        this.requestFrameId = requestAnimationFrame(this.renderScene);
+    }
+
+    initPlayer(player) {
+        const ctx = this.ctx;
+        const offsetByY = this.canvas.height / 40;
+        const heroWidth = this.canvas.width / 10;
+        const heroHeight = this.canvas.width / 10;
+        console.log('initPlayer');
+        
+        ctx.fillStyle = 'red';
+        this.player = player;
+        // ctx.fillStyle = player.sprite;
+        ctx.fillRect((this.canvas.width - heroWidth) / 2, this.canvas.height - heroHeight - offsetByY,
+            heroWidth, heroHeight);
+    }
+
+    initGhosts(ghosts) {
+        const ctx = this.ctx;
         const offsetByY = this.canvas.height / 40;
         const ghostWidth = this.canvas.width / 10;
         const ghostHeight = this.canvas.width / 10;
-
         
-        ctx.fillStyle = this.ghosts.first.color;
+        ctx.fillStyle = ghosts.first.color;
         ctx.fillRect((this.canvas.width - ghostWidth) / 16, this.canvas.height - ghostHeight - offsetByY, ghostWidth, ghostHeight);
 
-        ctx.fillStyle = this.ghosts.second.color;
+        ctx.fillStyle = ghosts.second.color;
         ctx.fillRect((this.canvas.width - ghostWidth) * 15 / 16, this.canvas.height - ghostHeight - offsetByY, ghostWidth, ghostHeight);
-        // scene.render();
-
-        // ghostsInside.forEach(ghost => scene.remove(ghost));
-        this.requestFrameId = requestAnimationFrame(this.renderScene);
     }
 
     start() {
