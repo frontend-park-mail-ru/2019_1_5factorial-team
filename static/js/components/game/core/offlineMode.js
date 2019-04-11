5a2d633cc83f94ba14a197b2e9f228dc731b787b
nmerk <nadyamerk@gmail.com>
2019-04-11 12:07:45 +0300

    game-logic rewritten

--- static/js/components/game/core/offlineMode.js
+++ static/js/components/game/core/offlineMode.js
@@ -1,36 +1,10 @@
-import GameCore from './gameCore.js';
-// const Ws = require('Ws');    
-// const ws = new Ws();
-
-export default class offlineGame extends GameCore {
-    start() {
-        super.start();
-        console.log('inside got off');
-        // ws.send('start-game', null);
-        this.scene.start();
-    }
-
-    // onControllsPressed(evt) {
-    //     if (this._pressed('LEFT', evt)) {
-    //         ws.send('game-command', 'LEFT');
-    //     } else if (this._pressed('RIGHT', evt)) {
-    //         ws.send('game-command', 'RIGHT');
-    //     } else if (this._pressed('FIRE', evt)) {
-    //         ws.send('game-command', 'FIRE');
-    //     }
-    // }
-
-    onGameStarted() {
-        // this.controller.start();
-        // this.scene.init(evt);
-        this.scene.start();
-    }
-
-    // onGameFinished(evt) {
-    //     bus.emit('CLOSE_GAME');
-    // }
-
-    // onGameStateChanged(evt) {
-    //     this.scene.setState(evt);
-    // }
-}
+// import GameManager from '../gameManager';
+//
+// export default class OfflineGame {
+//     init() {
+//         this.manager = new GameManager(this.mode);
+//         this.manager.onStart();
+//
+//         console.log('inside got off');
+//     }
+// }
--- static/js/components/game/gameScene.js
+++ static/js/components/game/gameScene.js
@@ -1,83 +1,67 @@
-export default class gameScene {
-    constructor(canvas, ghosts = {}) {
-        this.canvas = canvas;
-        this.ghosts = ghosts;
-        this.state = null;
-        this.bindedResizer = this.resizer.bind(this); // TODO(): переехать на шину событий, хезе как
+export default class GameScene {
+    constructor(canvas) {
+        console.log('GameScene called');
 
+        this.canvas = canvas;
+
+        this.ctx = this.canvas.getContext('2d');
+
+        this.bindedResizer = this.resizer.bind(this); // TODO(): переехать на шину событий, хезе как
         window.addEventListener('resize', this.bindedResizer);
         this.resizer();
 
-        this.ctx = this.canvas.getContext('2d');
-        this.renderScene = this.renderScene.bind(this);
+        this.renderScene();
     }
 
-    renderScene(now) {
-        this.lastFrameTime = now;
-        console.log('got inside gs');
-        this.initGhosts(this.ghosts);
-        this.initPlayer(this.player);
-    }
-
-    initPlayer(player) {
+    renderScene() {
         const ctx = this.ctx;
+        console.log('render scene called');
+
+        // смещение по Y - расстояние от нижнего края экрана
         const offsetByY = this.canvas.height / 40;
 
+        // hero
         const heroX = this.canvas.width / 2;
         const heroY = this.canvas.height - offsetByY;
 
-        console.log('initPlayer');
-
         let heroImg = new Image();
         heroImg.onload = function() {
-            ctx.beginPath();
+            console.log('render hero called');
             ctx.drawImage(heroImg, heroX - heroImg.width / 2, heroY - heroImg.height);
         };
         heroImg.src = '../../../img/game/hero.png';
 
-        this.player = player;
-    }
-
-    initGhosts(ghosts) {
-        const ctx = this.ctx;
-        const offsetByY = this.canvas.height / 40;
+        // ghosts
         const ghostX = this.canvas.width / 16;
         const ghostY = this.canvas.height - offsetByY;
 
         let ghostLeftImg = new Image();
         ghostLeftImg.onload = function() {
-            ghosts.first.img = ghostLeftImg;
-            ctx.beginPath();
+            console.log('render ghost_l called');
             ctx.drawImage(ghostLeftImg, ghostX - ghostLeftImg.width / 16, ghostY - ghostLeftImg.height);
+            console.log('ghost_l img drawn: ' + '(' + ghostLeftImg.x + ', ' + ghostLeftImg.y + '), W: ' +  ghostLeftImg.width + ' H: ' + ghostLeftImg.height);
         };
         ghostLeftImg.src = '../../../img/game/ghost_l.png';
 
         let ghostRightImg = new Image();
         ghostRightImg.onload = function() {
-            ghosts.second.img = ghostRightImg;
-            ctx.beginPath();
             ctx.drawImage(ghostRightImg, (ghostX - ghostRightImg.width / 16) * 15, ghostY - ghostRightImg.height);
+            console.log('ghost_r img drawn: ' + '(' + ghostRightImg.x + ', ' + ghostRightImg.y + '), W: ' +  ghostRightImg.width + ' H: ' + ghostRightImg.height);
         };
         ghostRightImg.src = '../../../img/game/ghost_r.png';
-    }
 
-    start() {
-        console.log('got inside start');
-        this.lastFrameTime = performance.now();
-        this.requestFrameId = requestAnimationFrame(this.renderScene);
+        // requestAnimationFrame(this.renderScene.bind(this));
     }
 
     resizer() {
         this.canvas.height = window.innerHeight;
         this.canvas.width = window.innerWidth;
+
+        console.log('resized H: ' + this.canvas.height);
+        console.log('resized W: ' + this.canvas.width);
     }
 
-    stop() {
-        if (this.requestFrameId) {
-            window.cancelAnimationFrame(this.requestFrameId);
-            this.requestFrameId = null;
-        }
-
-        this.scene.clear();
+    destroy() {
+        window.removeEventListener('resize', this.bindedResizer);
     }
 }
